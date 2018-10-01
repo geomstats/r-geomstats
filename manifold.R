a56065afa811fe4654489b386f06580430d5fbf4
lundjack <35204227+lundjack@users.noreply.github.com>
2018-10-01 12:24:15 -0700

    Add files via upload

---
+++ manifold.R
@@ -0,0 +1,30 @@
+#Manifold, i.e. a topological space that locally resembles
+#Euclidean space near each point.
+
+library(reticulate)
+gs <- import_from_path("geomstats", path = ".")
+
+Manifold <- setRefClass("Manifold",
+                        methods = list(
+
+                          init <- function(self, dimension){
+                            stopifnot(dimension %% 1 == 0, dimension > 0)
+                            self$dimension = dimension
+                          },
+
+                          belongs <- function(self, point){
+                            # Evaluate if a point belongs to the manifold.
+
+                            # raise NotImplementedError('belongs is not implemented.')
+                          },
+
+                          regularize <- function(self, point){
+                            # Regularize a point to the canonical representation
+                            # chosen for the manifold.
+
+                            # raise NotImplementedError('regularize is not implemented.')
+                          }
+
+
+                        )
+                        )
